19e46d55963840ff3c80d2b6977c817c8127ea5f
Darya A <dashulyaalex@mail.ru>
2022-09-24 16:30:51 +0300

    add index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+import { car, cdr } from '@hexlet/pairs';
+
+const allGamesEngine = (gameName, gameQuestion, gameContent) => {
+  console.log(`${gameName}\n\nWelcome to the Brain Games!'`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(`${gameQuestion}`);
+  let numberOfCorrectAnswers = 0;
+  while (numberOfCorrectAnswers < 3) {
+    const exerciseAnswerSet = gameContent();
+    console.log(`Question: ${car(exerciseAnswerSet)}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = cdr(exerciseAnswerSet);
+    if (userAnswer === correctAnswer) {
+      numberOfCorrectAnswers += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      break;
+    }
+  }
+};
+
+export default allGamesEngine;
