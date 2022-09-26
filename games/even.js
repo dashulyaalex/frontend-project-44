bd3bf3f72241cf4654f76b7a9c0489f9f387f805
Darya A <dashulyaalex@mail.ru>
2022-09-26 11:04:57 +0300

    update games/even.js

--- games/even.js
+++ games/even.js
@@ -6,12 +6,7 @@ const evenGame = () => {
   const gameQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
   const exercise = () => {
     const number = getRandomInterger(0, 100);
-    let correctAnswer = '';
-    if ((number % 2) === 0) {
-      correctAnswer = 'yes';
-    } else {
-      correctAnswer = 'no';
-    }
+    const correctAnswer = (number % 2) === 0 ? 'yes' : 'no';
     const result = cons(number, correctAnswer);
     return result;
   };
