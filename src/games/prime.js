a69f233cc8a4fcfcf1b0a13bde0cff5f3664faf7
Darya A <dashulyaalex@mail.ru>
2022-10-04 16:05:32 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,7 +3,7 @@ import allGamesEngine from '../index.js';
 import getRandomInterger from '../getRandom.js';
 
 const isPrime = (num) => {
-  for (let i = 2; i < Math.sqrt(num); i += 1) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
